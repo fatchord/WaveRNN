024f6367cdc29cd7a4c11032cb812ef09022e2d9
fatchord <fatchord@tutanota.com>
2019-04-01 16:02:01 +0100

    Add BinnedLengthSampler()

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,10 +1,27 @@
 import pickle
+import random
 import torch
 from torch.utils.data import Dataset, DataLoader
 from utils.dsp import *
 import hparams as hp
 
-def get_datasets(path, batch_size=16) :
+
+class VocoderDataset(Dataset):
+    def __init__(self, ids, path):
+        self.path = path
+        self.metadata = ids
+
+    def __getitem__(self, index):
+        file = self.metadata[index]
+        m = np.load(f'{self.path}mel/{file}.npy')
+        x = np.load(f'{self.path}quant/{file}.npy')
+        return m, x
+
+    def __len__(self):
+        return len(self.metadata)
+
+
+def get_vocoder_datasets(path, batch_size=16) :
 
     with open(f'{path}dataset_ids.pkl', 'rb') as f:
         dataset_ids = pickle.load(f)
@@ -12,8 +29,8 @@ def get_datasets(path, batch_size=16) :
     test_ids = dataset_ids[-hp.voc_test_samples:]
     train_ids = dataset_ids[:-hp.voc_test_samples]
 
-    train_dataset = AudiobookDataset(train_ids, path)
-    test_dataset = AudiobookDataset(test_ids, path)
+    train_dataset = VocoderDataset(train_ids, path)
+    test_dataset = VocoderDataset(test_ids, path)
 
     train_set = DataLoader(train_dataset,
                            collate_fn=collate,
@@ -31,23 +48,7 @@ def get_datasets(path, batch_size=16) :
     return train_set, test_set
 
 
-class AudiobookDataset(Dataset):
-    def __init__(self, ids, path):
-        self.path = path
-        self.metadata = ids
-
-    def __getitem__(self, index):
-        file = self.metadata[index]
-        m = np.load(f'{self.path}mel/{file}.npy')
-        x = np.load(f'{self.path}quant/{file}.npy')
-        return m, x
-
-    def __len__(self):
-        return len(self.metadata)
-
-
-
-def collate(batch):
+def collate_vocoder(batch):
     mel_win = hp.voc_seq_len // hp.hop_length + 2 * hp.voc_pad
     max_offsets = [x[0].shape[-1] - (mel_win + 2 * hp.voc_pad) for x in batch]
     mel_offsets = [np.random.randint(0, offset) for offset in max_offsets]
@@ -60,11 +61,43 @@ def collate(batch):
     mels = np.stack(mels).astype(np.float32)
     labels = np.stack(labels).astype(np.int64)
 
-    mels = torch.FloatTensor(mels)
-    labels = torch.LongTensor(labels)
+    mels = torch.tensor(mels)
+    labels = torch.tensor(labels).long()
 
     x = label_2_float(labels[:, :hp.voc_seq_len].float(), hp.bits)
 
     y = labels[:, 1:]
 
     return x, y, mels
+
+
+class BinnedLengthSampler(Sampler):
+    def __init__(self, lengths, batch_size, bin_size):
+        _, self.idx = torch.sort(torch.tensor(lengths).long())
+        self.batch_size = batch_size
+        self.bin_size = bin_size if bin_size else batch_size * 4
+        assert self.bin_size % self.batch_size == 0
+
+    def __iter__(self):
+        # Need to change to numpy since there's a bug in random.shuffle(tensor)
+        # TODO : Post an issue on pytorch repo
+        idx = self.idx.numpy()
+        bins = []
+
+        for i in range(len(idx) // self.bin_size):
+            this_bin = idx[i * self.bin_size:(i + 1) * self.bin_size]
+            random.shuffle(this_bin)
+            bins += [this_bin]
+
+        random.shuffle(bins)
+        binned_idx = np.stack(bins).reshape(-1)
+
+        if len(binned_idx) < len(idx):
+            last_bin = idx[len(binned_idx):]
+            random.shuffle(last_bin)
+            binned_indices = np.concatenate([binned_idx, last_bin])
+
+        return iter(torch.tensor(binned_idx).long())
+
+    def __len__(self):
+        return len(self.indices)
