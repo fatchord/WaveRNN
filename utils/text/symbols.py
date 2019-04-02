b5ef0e3066a5c341408e95cb80e66ee082cff2cf
fatchord <fatchord@tutanota.com>
2019-04-02 14:00:43 +0100

    fix whitespace

--- utils/text/symbols.py
+++ utils/text/symbols.py
@@ -6,7 +6,7 @@ Defines the set of symbols used in text input to the model.
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
 from utils.text import cmudict
 
-_pad        = '_'
+_pad = '_'
 _punctuation = '!\'(),.:;? '
 _special = '-'
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
