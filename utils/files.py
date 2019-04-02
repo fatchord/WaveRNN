9f8794c1b668a70d07fd2d695e1270549b766428
fatchord <fatchord@tutanota.com>
2019-04-02 12:45:53 +0100

    refactor get_files into utils.files

---
+++ utils/files.py
@@ -0,0 +1,7 @@
+import glob
+
+def get_files(path, extension='.wav') :
+    filenames = []
+    for filename in glob.iglob(f'{path}/**/*{extension}', recursive=True):
+        filenames += [filename]
+    return filenames
