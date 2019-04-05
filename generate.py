d118c7edf375d1923870da3394f2e739c13c170c
fatchord <fatchord@tutanota.com>
2019-04-05 12:27:27 +0200

    allow wavernn generate from GTA testset

--- generate.py
+++ generate.py
@@ -59,6 +59,7 @@ if __name__ == "__main__":
     parser.add_argument('--overlap', '-o', type=int, help='[int] number of crossover samples')
     parser.add_argument('--file', '-f', type=str, help='[string/path] for testing a wav outside dataset')
     parser.add_argument('--weights', '-w', type=str, help='[string/path] checkpoint file to load weights from')
+    parser.add_argument('--use_gta', '-g', dest='use_gta', action='store_true', help='Generate from GTA testset')
 
     parser.set_defaults(batched=hp.voc_gen_batched)
     parser.set_defaults(samples=hp.voc_gen_at_checkpoint)
@@ -66,6 +67,7 @@ if __name__ == "__main__":
     parser.set_defaults(overlap=hp.voc_overlap)
     parser.set_defaults(file=None)
     parser.set_defaults(weights=None)
+    parser.set_defaults(use_gta=False)
 
     args = parser.parse_args()
 
@@ -74,6 +76,7 @@ if __name__ == "__main__":
     target = args.target
     overlap = args.overlap
     file = args.file
+    use_gta = args.use_gta
 
     print('\nInitialising Model...\n')
 
@@ -99,7 +102,7 @@ if __name__ == "__main__":
                   ('Target Samples', target if batched else 'N/A'),
                   ('Overlap Samples', overlap if batched else 'N/A')])
 
-    _, test_set = get_vocoder_datasets(paths.data, 1, False)
+    _, test_set = get_vocoder_datasets(paths.data, 1, use_gta)
 
     if file :
         gen_from_file(model, file, paths.voc_output, batched, target, overlap)
